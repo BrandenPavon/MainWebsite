c7237d402c85daea89cac0a16ded541a1e3916a6
ch1llyspirit <notchillyspirit@gmail.com>
2023-09-06 10:16:35 -0700

    09/06/23

---
+++ README.md
@@ -0,0 +1,3 @@
+# MainWebsite
+
+Main Site
---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask, session
+from flask import render_template, send_from_directory
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+
+@app.route("/usefulinfo")
+def usefulinfo():
+    return render_template('usefulinfo.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
